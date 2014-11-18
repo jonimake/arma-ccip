448d0155ab2ec1dbc1582920f3cb4841af5d17e3
eRazeri <eraserfrompaikkala@gmail.com>
2014-11-18 17:28:58 +0200

    Revert "updated"
    This reverts commit 826e32c37e2b176632ca1a0ea37fd2ee0506bf1c.

--- Addon versions/arma_ccip/config.cpp
+++ Addon versions/arma_ccip/config.cpp
@@ -14,11 +14,11 @@ class CfgFunctions
 	{
 		class ArmA_CCIP 
 		{
-      file = "functions";
-      class init {postInit = 1;};
-      class getImpactPos;
-      class getImpactPosBomb;
-      class getImpactPosRocket;
+			class _init
+			{
+				file = "\ArmA_CCIP\init.sqf";
+				postInit = 1;
+			};
 		};
 	};
 };
