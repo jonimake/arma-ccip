0a6a578dec0f7df25bd8c898571f8a1901e347ff
eRazeri <eraserfrompaikkala@gmail.com>
2014-12-11 21:50:40 +0200

    Updated to v0.2a

--- addons/123_automatic_signer.cmd
+++ addons/123_automatic_signer.cmd
@@ -3,6 +3,6 @@
 SETLOCAL ENABLEDELAYEDEXPANSION
 
 SET DSSignFile="C:\Steam\SteamApps\common\Arma 3 Tools\DSSignFile\DSSignFile.exe"    
-SET PRIVATEKEY="C:\Steam\SteamApps\common\Arma 3 Tools\DSSignFile\privakeys\CCIP_v0.1a.biprivatekey"
+SET PRIVATEKEY="C:\Steam\SteamApps\common\Arma 3 Tools\DSSignFile\privakeys\CCIP_v0.2a.biprivatekey"
 
 FOR %%i IN (*.pbo) DO %DSSignFile% %PRIVATEKEY% "%CD%\%%i"
